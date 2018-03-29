051f0ba071c4cebbbf279724908932c2907a5bea
Pavlo Kuchereshko <atos.ua@gmail.com>
2018-03-29 16:08:23 +0300

    vanilla

---
+++ app/src/main/java/com/pk/vanilla/util/ApiUtil.java
@@ -0,0 +1,8 @@
+package com.pk.vanilla.util;
+
+public class ApiUtil {
+    public static final String API_KEY = "8499934-51ca6dfffe38c79d79c24afc0";
+    public static final String QUERY_PREFIX = "https://pixabay.com/api/?key=";
+    public static final String QUERY_PARAMETER = "&q=";
+    public static final String QUERY_SUFIX = "&image_type=photo";
+}
